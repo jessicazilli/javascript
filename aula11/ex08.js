ef4dd9a65b9f967e647b1a8c4ebab55970d86ed6
Jéssica Zilli <91jessicazilli@gmail.com>
2025-01-22 20:10:02 -0300

    Criei a aula 10

---
+++ aula11/ex08.js
@@ -0,0 +1,10 @@
+var vel = 80.5
+console.log(`A velocidade do seu carro é ${vel}km/h!`)
+// console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+// console.log(`Dirija sempre usando cinto de segurança!`)
+
+if (vel >= 80) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} else {
+    console.log(`Dirija sempre usando cinto de segurança!`)
+}
