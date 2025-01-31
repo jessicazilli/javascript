7920bd1fad0a030ab016f448b0630e2117fa4514
Jéssica Zilli <91jessicazilli@gmail.com>
2025-01-31 18:34:38 -0300

    Atualizei os exercícios da aula 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+
+
+ 
+/*
+
+var c = 1
+while (c < 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
+
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
